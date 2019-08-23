a7aa6efe2466d2276c4ef4036f00d3b5dc842e80
Tringali, Joe <tringj@amazon.com>
2019-08-23 15:52:51 -0400

    Handle both create and update

--- scripts/create-or-update-vpc.sh
+++ scripts/create-or-update-vpc.sh
@@ -14,11 +14,11 @@ name_tag=""
 
 s="Usage: $script_name"
 s+="  --stack-name <The stack name>"
-s+=" [ --cidr_block <The primary IPv4 CIDR block>"
-s+=" --enable_dns_hostnames <true or false>"
-s+=" --enable_dns_support <true or false>"
-s+=" --instance_tenancy <dedicated, default or host"
-s+=" --name_tag <value of 'Name' tag>"
+s+=" [ --cidr-block <The primary IPv4 CIDR block>"
+s+=" --enable-dns-hostnames <true or false>"
+s+=" --enable-dns-support <true or false>"
+s+=" --instance-tenancy <dedicated, default or host"
+s+=" --name-tag <value of 'Name' tag>"
 s+=" --region <The region for the stack>"
 s+=" --help ]"
 
@@ -29,11 +29,11 @@ display_usage() {
     echo ""
     echo "  --help                  Display help."
     echo "  --stack-name            The name that is associated with the stack. The name must be unique in the region in which you are creating the stack."
-    echo "  --cidr_block            The primary IPv4 CIDR block for the VPC."
-    echo "  --enable_dns_hostnames  Indicates whether the instances launched in the VPC get DNS hostnames."
-    echo "  --enable_dns_support    Indicates whether the DNS resolution is supported for the VPC."
-    echo "  --instance_tenancy      The allowed tenancy of instances launched into the VPC."
-    echo "  --name_tag              A tag with a key of 'Name' and a value that you specify."
+    echo "  --cidr-block            The primary IPv4 CIDR block for the VPC."
+    echo "  --enable-dns-hostnames  Indicates whether the instances launched in the VPC get DNS hostnames."
+    echo "  --enable-dns-support    Indicates whether the DNS resolution is supported for the VPC."
+    echo "  --instance-tenancy      The allowed tenancy of instances launched into the VPC."
+    echo "  --name-tag              A tag with a key of 'Name' and a value that you specify."
     echo "  --region                <The region for the stack>"
     echo ""
 }
@@ -45,19 +45,19 @@ while (( "$#" )); do
     elif [[ (( $1 == "--stack-name") || $1 == "-s") && $# -ge 2 ]]; then
         shift
         stack_name=$1
-    elif [[ (( $1 == "--cidr_block") || $1 == "-c") && $# -ge 2 ]]; then
+    elif [[ (( $1 == "--cidr-block") || $1 == "-c") && $# -ge 2 ]]; then
         shift
         cidr_block=$1
-    elif [[ (( $1 == "--enable_dns_hostnames") || $1 == "-edh") && $# -ge 2 ]]; then
+    elif [[ (( $1 == "--enable-dns-hostnames") || $1 == "-edh") && $# -ge 2 ]]; then
         shift
         enable_dns_hostnames=$1
-    elif [[ (( $1 == "--enable_dns_support") || $1 == "-eds") && $# -ge 2 ]]; then
+    elif [[ (( $1 == "--enable-dns-support") || $1 == "-eds") && $# -ge 2 ]]; then
         shift
         enable_dns_support=$1
-    elif [[ (( $1 == "--instance_tenancy") || $1 == "-i") && $# -ge 2 ]]; then
+    elif [[ (( $1 == "--instance-tenancy") || $1 == "-i") && $# -ge 2 ]]; then
         shift
         instance_tenancy=$1
-    elif [[ (( $1 == "--name_tag") || $1 == "-n") && $# -ge 2 ]]; then
+    elif [[ (( $1 == "--name-tag") || $1 == "-n") && $# -ge 2 ]]; then
         shift
         name_tag=$1
     elif [[ (( $1 == "--region") || $1 == "-r") && $# -ge 2 ]]; then
@@ -75,7 +75,13 @@ if [[ (-z $stack_name) ]]; then
     exit 1
 fi
 
-$aws_cmd cloudformation create-stack \
+if $aws_cmd cloudformation describe-stacks --region $region --stack-name $stack_name> /dev/null 2>&1 ; then
+    create_or_update='update'
+else
+    create_or_update="create"
+fi
+
+$aws_cmd cloudformation ${create_or_update}-stack \
         --region $region \
         --stack-name $stack_name \
         --template-body file://../templates/vpc.yaml \
